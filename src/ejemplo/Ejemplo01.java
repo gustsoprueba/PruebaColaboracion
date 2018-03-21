e9e49813728f2f6ea8fc27556089b12c7236239c
gustso <gustso@192.168.1.100>
2018-03-21 12:19:06 -0300

    Inicializo el Trabajo y agrego el proyecto con archivos Java

---
+++ src/ejemplo/Ejemplo01.java
@@ -0,0 +1,22 @@
+/**
+ * JAVA DOC
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ **/
+package ejemplo;
+
+/**
+ *
+ * @author gustso
+ */
+public class Ejemplo01 {
+    
+    public static void saludarAlPublico(String nombreSaluda){
+        System.out.println("Hola Programacion Visual "+nombreSaluda);
+    }
+            
+    public static void main(String[] arg){
+        saludarAlPublico("Gustavo Sosa");
+    }
+}
